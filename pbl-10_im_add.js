149bc5d042efe2b6b7b914ee51863bd57031cd39
TerashimaYuya <67239827+TerashimaYuya@users.noreply.github.com>
2020-07-14 01:42:51 +0900

    Update pbl-10_im_add.js

--- pbl-10_im_add.js
+++ pbl-10_im_add.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
 	var kokki_num = parseInt(window.innerWidth / 200);
 	console.log(kokki_num);
-	var img = "<img src=\"https://github.com/TerashimaYuya/pbl-10/blob/master/usa.jpg?raw=true\" width=\"" + (100 / kokki_num) + "%\">";
+	var img = "<img src=\"https://raw.githubusercontent.com/TerashimaYuya/pbl-10/master/usa.jpg\" width=\"" + (100 / kokki_num) + "%\">";
 	var div = document.getElementById("pbl-10_im_add");
 	var text = "";
 	for (var i = 0; i < kokki_num; ++i) {
